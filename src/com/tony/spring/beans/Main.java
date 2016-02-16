8ab214796d56b382a8b49e0ff0228183da276dc8
XidongHuang <gzink@live.cn>
2016-02-15 23:54:29 -0500

    Spring 4.2.4: HelloWorld

---
+++ src/com/tony/spring/beans/Main.java
@@ -0,0 +1,13 @@
+package com.tony.spring.beans;
+
+public class Main {
+
+	
+	public static void main(String[] args) {
+		
+		HelloWorld helloWorld = new HelloWorld();
+		helloWorld.setName("Tony");
+		helloWorld.hello();
+		
+	}
+}
