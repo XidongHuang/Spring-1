f6b6ad3153b6cf18de37185b63be5f77afdabc1c
XidongHuang <gzink@live.cn>
2016-02-17 22:59:52 -0500

    Spring 4.2.4
    Generic injection

---
+++ src/com/tony/spring/generic/di/User.java
@@ -0,0 +1,5 @@
+package com.tony.spring.generic.di;
+
+public class User {
+
+}
---
+++ src/com/tony/spring/generic/di/UserService.java
@@ -0,0 +1,8 @@
+package com.tony.spring.generic.di;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserService extends BaseService<User> {
+
+}
